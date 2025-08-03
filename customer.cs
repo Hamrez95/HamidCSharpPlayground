be7595fd02c553ea5c72917c1fb120d380e7999d
Hamid Reza Pakpour <h.pakpour@PAKPOUR119>
2025-08-03 13:20:22 +0330

    Add Customer class with ability to inject and switch discount strategy (Dependency Injection, Encapsulation)

---
+++ customer.cs
@@ -0,0 +1,27 @@
+﻿using HamidCSharpPlayground.Strategies;
+
+namespace HamidCSharpPlayground
+{
+    public class customer
+    {
+        public string Name { get; set; }
+        private IDiscountStrategy _discountStrategy;
+
+        public customer(string name, IDiscountStrategy discountStrategy)
+        {
+            Name = name;
+            _discountStrategy = discountStrategy;
+        }
+
+        public void SetDiscountStrategy(IDiscountStrategy strategy)
+        {
+            _discountStrategy = strategy;
+        }
+
+        public decimal GetPayableAmount(decimal totalAmount)
+        {
+            var discount = _discountStrategy.CalculateDiscount(totalAmount);
+            return totalAmount - discount;
+        }
+    }
+}
