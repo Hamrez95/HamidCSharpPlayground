73f3dab7a82b5649047da086b7a68e020d9a7ea6
Hamid Reza Pakpour <h.pakpour@PAKPOUR119>
2025-08-26 12:15:55 +0330

    implement factory pattern

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 ﻿using HamidCSharpPlayground.Strategies;
+using static HamidCSharpPlayground.Program;
 
 namespace HamidCSharpPlayground
 {
     internal class Program
     {
+        internal enum Plan
+        {
+            Employee,
+            Gold,
+            Loyal
+        }
         static void Main(string[] args)
         {
             var total = 100m;
@@ -12,32 +19,54 @@ namespace HamidCSharpPlayground
             var customerName = Console.ReadLine();
 
             Console.WriteLine("Please choose your Plan : Employee, Gold, Loyal");
-            var plan = Console.ReadLine();
+            var strPlan = Console.ReadLine();
+            Plan plan = (Plan)Enum.Parse(typeof(Plan), strPlan);
 
-            IDiscountStrategy discountStrategy;
+            IDiscountStrategy strategy = PlanFactory.Create(plan);
+            var customer1 = new Customer(customerName, strategy);
 
-            if(plan == "Employee")
-            {
-                discountStrategy = new EmployeeDiscount();
-            }
-            else if (plan == "Gold")
-            {
-                discountStrategy = new GoldCustomerDiscount();
-            }
-            else if (plan == "Loyal")
-            {
-                discountStrategy = new LoyalCustomerDiscount();
-            }
-            else
-            {
-                discountStrategy = new NoDiscount();
-            }
+            Console.WriteLine($"{customer1.Name} should pay {customer1.GetPayableAmount(total)} Toman");
+        }
+    }
 
-            var customer1 = new Customer(customerName, discountStrategy);
-         
+    internal static class PlanFactory
+    {
+        //public PlanFactory(string plan)
+        //{
+        //    switch (plan)
+        //    {
+        //        case "Employee":
+        //            Strategy = new EmployeeDiscount();
+        //            break;
+        //        case "Gold":
+        //            Strategy = new GoldCustomerDiscount();
+        //            break;
+        //        case "Loyal":
+        //            Strategy = new LoyalCustomerDiscount();
+        //            break;
+        //        default:
+        //            Strategy = new NoDiscount();
+        //            break;
+        //    }
+        //}
 
-            Console.WriteLine($"{ customer1.Name} should pay { customer1.GetPayableAmount(total)} Toman");
-        
+        //public IDiscountStrategy Strategy { get; }
+
+
+        public static IDiscountStrategy Create(Plan plan)
+        {
+            switch (plan)
+            {
+                case Plan.Employee:
+                    return new EmployeeDiscount();
+                case Plan.Gold:
+                    return new GoldCustomerDiscount();
+                case Plan.Loyal:
+                    return new LoyalCustomerDiscount();
+                default:
+                    return new NoDiscount();
+            }
         }
     }
 }
+
