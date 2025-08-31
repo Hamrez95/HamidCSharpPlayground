6abded68c101c859d5a3ee2ae8f0fba27996eced
Hamid Reza Pakpour <h.pakpour@PAKPOUR119>
2025-08-31 11:44:50 +0330

    Create snake Drawboard

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SnakeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(10, 5);
+            Console.Write("#");
+
+            DrawBoard();
+
+            void DrawBoard()
+            {
+                int width = 50;
+                int height = 20;
+
+                Console.Clear(); 
+
+                // خط بالا
+                for (int x = 0; x < width; x++)
+                {
+                    Console.SetCursorPosition(x, 0);
+                    Console.Write("#");
+                }
+
+                // خط پایین
+                for (int x = 0; x < width; x++)
+                {
+                    Console.SetCursorPosition(x, height - 1);
+                    Console.Write("#");
+                }
+
+                // خط چپ
+                for (int y = 0; y < height; y++)
+                {
+                    Console.SetCursorPosition(0, y);
+                    Console.Write("#");
+                }
+
+                // خط راست
+                for (int y = 0; y < height; y++)
+                {
+                    Console.SetCursorPosition(width - 1, y);
+                    Console.Write("#");
+                }
+            }
+        }
+
+
+    }
+}
